72632040e5067a931c7264dfb57aced8767cefe0
christyljoyreyes <christyl.joy@r4pid.com>
2024-09-27 20:14:24 +0800

    Vue Dashboard fix notfound layout

--- src/router.js
+++ src/router.js
@@ -3,10 +3,38 @@ import Dashboard from "./views/Dashboard.vue";
 import Table from "./views/Table.vue";
 import NotFound from "./views/NotFound.vue";
 
+const RouteName = {
+  Dashboard: "Dashboard",
+  Table: "Table",
+  NotFound: "NotFound",
+};
+
 const routes = [
-  { path: "/dashboard", component: Dashboard },
-  { path: "/table", component: Table },
-  { path: "/:pathMatch(.*)*", component: NotFound },
+  {
+    path: "/",
+    redirect: { name: RouteName.Dashboard },
+    children: [
+      {
+        path: "table",
+        name: RouteName.Table,
+        component: Table,
+      },
+      {
+        path: "dashboard",
+        name: RouteName.Dashboard,
+        component: Dashboard,
+      },
+      {
+        path: "404",
+        name: RouteName.NotFound,
+        component: NotFound,
+      },
+    ],
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    redirect: { name: RouteName.NotFound },
+  },
 ];
 
 const router = createRouter({
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,7 @@
 <template>
   <Teleport to="body">
     <div
-      class="fixed flex flex-col items-center justify-center w-full h-full top-0 container mx-auto px-6 py-8 font-medium bg-slate-900"
+      class="fixed flex flex-col items-center justify-center w-full h-full top-0 left-0 mx-0 px-6 py-8 font-medium bg-slate-900"
     >
       <div class="text-3xl text-white">Error Page</div>
       <router-link to="/dashboard" class="m-4 bg-blue-200 p-4 rounded-xl">
